a3b684276488cb2480e68098fa39e62415160ecc
Hubert Wiktorowicz <hubertwiktorowicz1@gmail.com>
2026-03-23 15:47:00 +0100

    printf

---
+++ src/Printf.java
@@ -0,0 +1,41 @@
+public class Printf {
+    public static void main(String[] args) {
+
+
+//
+//        String name = "Spongebob";
+//        char firstLetter = 'S';
+//        int age = 30;
+//        double height = 60.5;
+//        boolean isEmployed = true;
+//
+//        System.out.printf("Hello %s\n", name);
+//        System.out.printf("Your name starts with a %c\n", firstLetter);
+//        System.out.printf("You are %d years old\n", age);
+//        System.out.printf("You are %.1f inches tall\n", height);
+//        System.out.printf("Are you employed? : %b\n", isEmployed);
+//
+//        System.out.printf("%s is %d years old", name, age);
+//
+
+//
+//        double price1 = 9.99;
+//        double price2 = 100.15;
+//        double price3 = -54.01;
+//
+//        System.out.printf("% (.2f\n", price1);
+//        System.out.printf("% (.2f\n", price2);
+//        System.out.printf("% (.2f\n", price3);
+
+        int id1 = 1;
+        int id2 = 23;
+        int id3 = 456;
+        int id4 = 7890;
+
+        System.out.printf("%4d\n", id1);
+        System.out.printf("%4d\n", id2);
+        System.out.printf("%4d\n", id3);
+        System.out.printf("%4d\n", id4);
+
+    }
+}
