25a3fca5b9d57119610c01422d075c55808c119f
Hubert Wiktorowicz <hubertwiktorowicz1@gmail.com>
2026-03-23 14:03:04 +0100

    nowa klasa

---
+++ src/Randomowe.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class Randomowe{
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        boolean isHeads;
+
+        isHeads = random.nextBoolean();
+
+
+        //System.out.println(isHeads);
+
+        if(isHeads){
+            System.out.println("You won");
+        }
+        else{
+            System.out.println("You lost");
+        }
+    }
+
+}
