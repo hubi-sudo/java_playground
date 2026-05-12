d26e03919cb7b526326dcb2e03ebd5d109c6ab42
Hubert Wiktorowicz <hubertwiktorowicz1@gmail.com>
2026-05-12 19:53:51 +0200

    OOP excercises

---
+++ src/OOP/Garnek.java
@@ -0,0 +1,58 @@
+package OOP;
+
+public class Garnek {
+
+    int srednica;
+    int wysokosc;
+    String kolor;
+
+    public Garnek() {
+
+    }
+
+    public Garnek(int srednica, int wysokosc) {
+        this.srednica = srednica;
+        this.wysokosc = wysokosc;
+    }
+
+    public Garnek(int srednica, int wysokosc, String kolor) {
+        this.srednica = srednica;
+        this.wysokosc = wysokosc;
+        this.kolor = kolor;
+    }
+
+    public String gotuj() {
+        return "Gotowanie w trakcie";
+    }
+
+    public String gotuj(boolean czyDodalismySol)  {
+        if (czyDodalismySol) {
+            return "Gotowanie z solą";
+        }
+        else {
+            return "Gotowanie bez soli";
+        }
+    }
+
+    public int zwrocTemperatureWrzeniaWody(){
+        return 100;
+    }
+
+    public void gotujBezInformacji() {
+        wlaczGarnek();
+        gotujPrzezPolGodziny();
+        wylaczGarnek();
+    }
+    public void wlaczGarnek(){
+        System.out.println("Włączanie garnka");
+    }
+    public void gotujPrzezPolGodziny(){
+        System.out.println("Gotowanie przez 30 minut");
+    }
+    public void wylaczGarnek(){
+        System.out.println("Wylaczanie garnka");
+    }
+
+}
+
+
