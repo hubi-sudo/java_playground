fcfc4db3068ba19c5e9a17812a8fed4174728581
Hubert Wiktorowicz <hubertwiktorowicz1@gmail.com>
2026-05-15 18:36:12 +0200

    Slight changes

--- src/OOP/projekt/Main.java
+++ src/OOP/projekt/Main.java
@@ -12,22 +12,22 @@ import OOP.projekt.file.musicfile.MP3MusicFile;
 
 public class Main {
     public static void main(String[] args) {
-        GIFImageFile gif1 = new GIFImageFile("nazwa1.gif", 100);
-        JPGImageFile jpg1 = new JPGImageFile("nazwa1.jpg", 200, 80);
-
-        MP3MusicFile mp3file = new MP3MusicFile("plik.mp3", 4000, "Marilyn Manson",
-                "Broken Needle", 100);
-
-        Drive drive = new HDDDrive();
-        drive.addFile(gif1);
-        drive.addFile(jpg1);
-        drive.addFile((mp3file));
-
-        drive.listFiles();
-        File file = drive.findFile("plik.mp3");
-        System.out.println(file.getSize());
-
-
+//        GIFImageFile gif1 = new GIFImageFile("nazwa1.gif", 100);
+//        JPGImageFile jpg1 = new JPGImageFile("nazwa1.jpg", 200, 80);
+//
+//        MP3MusicFile mp3file = new MP3MusicFile("plik.mp3", 4000, "Marilyn Manson",
+//                "Broken Needle", 100);
+//
+//        Drive drive = new HDDDrive();
+//        drive.addFile(gif1);
+//        drive.addFile(jpg1);
+//        drive.addFile((mp3file));
+//
+//        drive.listFiles();
+//        File file = drive.findFile("plik.mp3");
+//        System.out.println(file.getSize());
+//
+        System.out.println(Monitor.getMaxHeight());
     }
 
 
--- src/OOP/projekt/Monitor.java
+++ src/OOP/projekt/Monitor.java
@@ -4,6 +4,13 @@ public class Monitor {
     private int width = 3840;
     private int height = 1920;
 
+    public static int MAX_HEIGHT = 3840;
+
+    public static int getMaxHeight() {
+        return MAX_HEIGHT;
+    }
+
+
     public void setLowResolution() {
         width = 800;
         height = 600;
