9fd64d34a42671e4e10ffdce7579c98ae41defca
Hubert Wiktorowicz <hubertwiktorowicz1@gmail.com>
2026-05-26 19:05:21 +0200

    Streams

---
+++ src/Streams/StreamsExample.java
@@ -0,0 +1,44 @@
+package Streams;
+
+import Streams.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamsExample {
+
+    // Lista pracowników jako pole klasy
+    private List<Employee> employees = new ArrayList<>();
+
+    // Metoda setup inicjalizująca dane (odpowiednik @BeforeEach bez JUnit)
+    public void setUp() {
+        Employee employee1 = new Employee("Honkytonk", "Cumbercooch", 25, List.of("Java", "Spring"));
+        Employee employee2 = new Employee("Brewery", "Chickenstrips", 31, List.of("C#", ".NET"));
+        Employee employee3 = new Employee("Beetlejuice", "Snugglesnatch", 23, List.of("JavaScript", "TypeScript"));
+        Employee employee4 = new Employee("Boobytrap", "Humperdinck", 42, List.of("PHP", "Laravel"));
+        Employee employee5 = new Employee("Buttermilk", "Bumbersplat", 38, List.of("Java", "Kotlin"));
+        Employee employee6 = new Employee("Rinkydink", "Curdlesnoot", 30, List.of("C", "C++"));
+        Employee employee7 = new Employee("Cogglesnatch", "Splishnsplash", 27, List.of("Python", "Django"));
+        Employee employee8 = new Employee("Tiddleywomp", "Charmander", 51, List.of("Java", "Scala"));
+
+        employees.add(employee1);
+        employees.add(employee2);
+        employees.add(employee3);
+        employees.add(employee4);
+        employees.add(employee5);
+        employees.add(employee6);
+        employees.add(employee7);
+        employees.add(employee8);
+    }
+
+    // Metoda getter dająca dostęp do listy pracowników
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void firstStream() {
+        employees.stream()
+                .forEach(employee -> System.out.println(employee));
+    }
+
+}
