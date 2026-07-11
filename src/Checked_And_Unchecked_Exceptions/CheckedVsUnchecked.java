4c7cad9078c1e2c5288022cc5ff30fad2792f51c
Hubert Wiktorowicz <hubertwiktorowicz1@gmail.com>
2026-07-11 16:58:18 +0200

    TryCatch fnfe

--- src/Checked_And_Unchecked_Exceptions/CheckedVsUnchecked.java
+++ src/Checked_And_Unchecked_Exceptions/CheckedVsUnchecked.java
@@ -1,16 +1,24 @@
 package Checked_And_Unchecked_Exceptions;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class CheckedVsUnchecked {
     public static void main(String[] args) {
 
-    readFile("myFile.txt")
+    readFile("myFile.txt");
 
     }
 
     private static void readFile(String fileName) {
-        FileReader reader = new FileReader(fileName);
+        try {
+            FileReader reader = new FileReader(fileName);
+        }
+        catch (FileNotFoundException fnfe){
+            System.out.println("That file doesnt exist");
+        }
     }
+
+
 
 }
