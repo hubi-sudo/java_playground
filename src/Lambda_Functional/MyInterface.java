6043e137f58ee4930792d7773e2b0529bde5db18
Hubert Wiktorowicz <hubertwiktorowicz1@gmail.com>
2026-07-12 16:29:54 +0200

    lambda

---
+++ src/Lambda_Functional/MyInterface.java
@@ -0,0 +1,6 @@
+package Lambda_Functional;
+@FunctionalInterface
+public interface MyInterface {
+
+    public void message(String name, char symbol);
+}
---
+++ src/Lambda_Functional/lambda.java
@@ -0,0 +1,22 @@
+package Lambda_Functional;
+
+public class lambda {
+    public static void main(String[] args) {
+
+
+        String name = "Hubert";
+        char symbol = '!';
+        MyInterface myInterface = (x,y) -> {
+            System.out.println("Hello ");
+            System.out.println("It is a nice day " +x +y);
+        };
+
+
+        MyInterface myInterface2 = (x,y) -> {
+            System.out.println("Heello " + x + y);
+        };
+
+        myInterface2.message(name,symbol);
+
+    }
+}
